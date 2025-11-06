6a83ea62ddce30ee83f2e9acd819b94f2280bece
Zairus Bermillo <166342742+notZairus@users.noreply.github.com>
2025-11-06 17:57:28 +0800

    sda

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      assetsInclude: ['**/*.glb'],
     },
   },
   assetsInclude: ['**/*.glb']
